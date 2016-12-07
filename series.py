d1ca12d785cb3d2812d42f8db088dafe93e72c63
Rick Valenzuela <rv@rickv.com>
2016-12-06 16:08:02 -0800

    Deleted commented-out code

--- series.py
+++ series.py
@@ -1,17 +1,6 @@
 """Fibonacci and Lucas Series Functions."""
 
 
-# def fibonacci(n):
-#     """Return the nth value of the fibonacci series."""
-#     if n < 1:
-#         return None
-#     elif n == 1:
-#         return 0
-#     elif n == 2:
-#         return 1
-#     else:
-#         return fibonacci(n - 1) + fibonacci(n - 2)
-
 def fibonacci(n):
     """Return nth value of Fibonacci series."""
     idx = 0
@@ -22,17 +11,6 @@ def fibonacci(n):
         a, b = b, a + b
         idx += 1
     return result[n - 1]
-
-# def lucas(n):
-#     """Return the nth value of the lucas series."""
-#     if n < 1:
-#         return None
-#     elif n == 1:
-#         return 2
-#     elif n == 2:
-#         return 1
-#     else:
-#         return lucas(n - 1) + lucas(n - 2)
 
 
 def lucas(n):
@@ -47,16 +25,6 @@ def lucas(n):
     return result[n - 1] 
 
 
-# def sum_series(n, first=1, second=2):
-#     """Return the nth value of a user initiated series."""
-#     if n < 1:
-#         return None
-#     elif n == 1:
-#         return first
-#     elif n == 2:
-#         return second
-#     else:
-#         return sum_series(n - 1) + sum_series(n - 2)
 
 def sum_series(n, first = 0, second = 1):
      """Return nth value of custom series."""
